f364a8e054d25c0421dd4c02fc2731bbabadb6b2
JhonHader <haderfh@gmail.com>
2020-11-12 19:31:53 -0500

    Description

--- Mask_Detector.py
+++ Mask_Detector.py
@@ -1,20 +1,67 @@
+''' Ruler 1         2         3         4         5         6         7        '
+/*******************************************************************************
+*                                                                              *
+*                  /$$      /$$                        /$$                     *
+*                 | $$$    /$$$                       | $$                     *
+*                 | $$$$  /$$$$   /$$$$$$    /$$$$$$$ | $$   /$$               *
+*                 | $$ $$/$$ $$  |____  $$  /$$_____/ | $$  /$$/               *
+*                 | $$  $$$| $$   /$$$$$$$ |  $$$$$$  | $$$$$$/                *
+*                 | $$\  $ | $$  /$$__  $$  \____  $$ | $$_  $$                *
+*                 | $$ \/  | $$ |  $$$$$$$  /$$$$$$$/ | $$ \  $$               *
+*                 |__/     |__/  \_______/ |_______/  |__/  \__/               *
+*                                                                              *
+*		 /$$$$$$$                /$$                              /$$          *
+*		| $$__  $$              | $$                             | $$          *
+*		| $$  \ $$   /$$$$$$   /$$$$$$     /$$$$$$    /$$$$$$$  /$$$$$$        *
+*		| $$  | $$  /$$__  $$ |_  $$_/    /$$__  $$  /$$_____/ |_  $$_/        *
+*		| $$  | $$ | $$$$$$$$   | $$     | $$$$$$$$ | $$         | $$          *
+*		| $$  | $$ | $$_____/   | $$ /$$ | $$_____/ | $$         | $$ /$$      *
+*		| $$$$$$$/ |  $$$$$$$   |  $$$$/ |  $$$$$$$ |  $$$$$$$   | $$$$/       *
+*		|_______/   \_______/    \___/    \_______/  \_______/    \___/        *
+*                                                                              *
+*                                                                              *
+*                  Developed by:                                               *
+*                                                                              *
+*                            Jhon Hader Fernandez                              *
+*                     - jhon_fernandez@javeriana.edu.co                        *
+*                                                                              *
+*                            Diego Fernando Diaz                               *
+*                        - di-diego@javeriana.edu.co                           *
+*                                                                              *
+*                          Oscar Geovanny Baracaldo                            *
+*                       - obaracaldo@javeriana.edu.co                          *
+*                                                                              *
+*                       Pontificia Universidad Javeriana                       *
+*                            Bogota DC - Colombia                              *
+*                                  Nov - 2020                                  *
+*                                                                              *
+*****************************************************************************'''
+
+#------------------------------------------------------------------------------#
+#                          IMPORT MODULES AND LIBRARIES                        #
+#------------------------------------------------------------------------------#
+
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
 import numpy as np
 import cv2
 
+
+#------------------------------------------------------------------------------#
+#                                  MASK DETECTOR                               #
+#------------------------------------------------------------------------------#
+
 predict = []
 
 maskDetectionModelPath = r".\Mask_Detect\mask_detector.model"
 maskNet = load_model(maskDetectionModelPath)
+
 def mask_detect(frame, bndbox):
 
-    bndbox = np.array(bndbox)
-
-    predict = [0,0,1]
-    if bndbox.shape == (1, 4):
-        #bndbox = bndbox[0]
+    if len(bndbox) >= 1:
+        bndbox = np.array(bndbox)
+        bndbox = bndbox[0]
         startX, startY, endX, endY = bndbox[0][0], bndbox[0][1], bndbox[0][2], bndbox[0][3]
 
         face = frame[startY:endY, startX:endX]
@@ -23,7 +70,6 @@ def mask_detect(frame, bndbox):
         face = img_to_array(face)
         face = preprocess_input(face)
         face = np.array([face])
-        #print(face.shape)
         predict = maskNet.predict(face)
 
     return [predict]
--- stream_mask_detection.py
+++ stream_mask_detection.py
@@ -1,51 +1,105 @@
+''' Ruler 1         2         3         4         5         6         7        '
+/*******************************************************************************
+*                                                                              *
+*	    /$$$$$$    /$$                                                         *
+* 	   /$$__  $$  | $$                                                         *
+* 	  | $$  \__/ /$$$$$$     /$$$$$$    /$$$$$$    /$$$$$$   /$$$$$$/$$$$      *
+* 	  |  $$$$$$ |_  $$_/    /$$__  $$  /$$__  $$  |____  $$ | $$_  $$_  $$     *
+* 	   \____  $$  | $$     | $$  \__/ | $$$$$$$$   /$$$$$$$ | $$ \ $$ \ $$     *
+* 	   /$$  \ $$  | $$ /$$ | $$       | $$_____/  /$$__  $$ | $$ | $$ | $$     *
+* 	  |  $$$$$$/  |  $$$$/ | $$       |  $$$$$$$ |  $$$$$$$ | $$ | $$ | $$     *
+* 	   \______/    \___/   |__/        \_______/  \_______/ |__/ |__/ |__/     *
+*                                                                              *
+*                  /$$      /$$                        /$$                     *
+*                 | $$$    /$$$                       | $$                     *
+*                 | $$$$  /$$$$   /$$$$$$    /$$$$$$$ | $$   /$$               *
+*                 | $$ $$/$$ $$  |____  $$  /$$_____/ | $$  /$$/               *
+*                 | $$  $$$| $$   /$$$$$$$ |  $$$$$$  | $$$$$$/                *
+*                 | $$\  $ | $$  /$$__  $$  \____  $$ | $$_  $$                *
+*                 | $$ \/  | $$ |  $$$$$$$  /$$$$$$$/ | $$ \  $$               *
+*                 |__/     |__/  \_______/ |_______/  |__/  \__/               *
+*                                                                              *
+*		 /$$$$$$$                /$$                              /$$          *
+*		| $$__  $$              | $$                             | $$          *
+*		| $$  \ $$   /$$$$$$   /$$$$$$     /$$$$$$    /$$$$$$$  /$$$$$$        *
+*		| $$  | $$  /$$__  $$ |_  $$_/    /$$__  $$  /$$_____/ |_  $$_/        *
+*		| $$  | $$ | $$$$$$$$   | $$     | $$$$$$$$ | $$         | $$          *
+*		| $$  | $$ | $$_____/   | $$ /$$ | $$_____/ | $$         | $$ /$$      *
+*		| $$$$$$$/ |  $$$$$$$   |  $$$$/ |  $$$$$$$ |  $$$$$$$   | $$$$/       *
+*		|_______/   \_______/    \___/    \_______/  \_______/    \___/        *
+*                                                                              *
+*                                                                              *
+*                  Developed by:                                               *
+*                                                                              *
+*                            Jhon Hader Fernandez                              *
+*                     - jhon_fernandez@javeriana.edu.co                        *
+*                                                                              *
+*                            Diego Fernando Diaz                               *
+*                        - di-diego@javeriana.edu.co                           *
+*                                                                              *
+*                          Oscar Geovanny Baracaldo                            *
+*                       - obaracaldo@javeriana.edu.co                          *
+*                                                                              *
+*                       Pontificia Universidad Javeriana                       *
+*                            Bogota DC - Colombia                              *
+*                                  Nov - 2020                                  *
+*                                                                              *
+*****************************************************************************'''
+
+#------------------------------------------------------------------------------#
+#                          IMPORT MODULES AND LIBRARIES                        #
+#------------------------------------------------------------------------------#
+
 from Face_Detector import *
 from Mask_Detector import *
 
-def draw_face(frame, bndbox, predict):
-	bndbox = np.array(bndbox)
 
-	if bndbox.shape == (1, 4):
-		#bndbox = bndbox[0]
+#------------------------------------------------------------------------------#
+#                          			  DRAW FACE                                #
+#------------------------------------------------------------------------------#
+
+def draw_face(frame, bndbox, predict, verbose=False):
+
+	if len(bndbox) >= 1:
+		bndbox = np.array(bndbox)
+		bndbox = bndbox[0]
 
 		predict = predict[0][0]
-		print(predict, type(predict))
 		index = np.where(predict == np.max(predict))[0][0]
-		print(index,predict[index])
+
+		if verbose == True:
+			print('Class: ', classes[index], 'Probability: ', predict[index])
+
 		class_predict = classes[index]
-		#print('class predict',class_predict, 'max', max(predict[0][0]))
 		color = classes_color[class_predict]
 
-		startX, startY, endX, endY = bndbox[0][0], bndbox[0][1], bndbox[0][2], bndbox[0][3]
+		startX, startY = bndbox[0][0], bndbox[0][1]
+		endX, endY = bndbox[0][2], bndbox[0][3]
 		start_point, end_point = (startX, startY), (endX, endY)
 
 		FONT, FONT_SCALE, thickness = cv2.FONT_HERSHEY_DUPLEX, 1.3, 2
 
-		(label_width, label_height) = cv2.getTextSize(class_predict, FONT, FONT_SCALE, thickness)[0]
+		(label_width, label_height) = cv2.getTextSize(class_predict, FONT,
+													  FONT_SCALE, thickness)[0]
 		org = ( (endX - startX)//2 + startX - label_width//2, startY - 10 )
 
 		frame = cv2.rectangle(frame, start_point, end_point, color, thickness)
-		frame = cv2.putText(frame, class_predict, org, FONT, FONT_SCALE, color, thickness)
+		frame = cv2.putText(frame, class_predict, org, FONT, FONT_SCALE, color,
+							thickness)
 
 	return frame
+
+
+#------------------------------------------------------------------------------#
+#                          			    MAIN                                   #
+#------------------------------------------------------------------------------#
 
 if __name__ == "__main__":
 
 	classes = ['mask_weared_incorrect','with_mask', 'without_mask']
-	classes_color = {'mask_weared_incorrect': (0,127,255),
-		'with_mask': (0, 255, 0),
-			  'without_mask': (0, 0, 255)
-			 }
-
-
-	# mal_puesta = cv2.imread('mal_puesto.jpg')
-	# bndbox = face_detect(mal_puesta)
-	# print(bndbox)
-	# predict = mask_detect(mal_puesta, bndbox)
-	# print(predict)
-	# frame = draw_face(mal_puesta, bndbox, predict)
-	# cv2.imshow('frmae',frame)
-	# cv2.waitKey(0)
-
+	classes_color = {'mask_weared_incorrect': (0, 127, 255),
+		             'with_mask': (0, 255, 0),
+			         'without_mask': (0, 0, 255)}
 
 	vid = cv2.VideoCapture(0)
 
@@ -54,8 +108,7 @@ if __name__ == "__main__":
 		ret, frame = vid.read()
 		bndbox = face_detect(frame)
 		predict = mask_detect(frame, bndbox)
-		print(predict)
-		frame = draw_face(frame, bndbox, predict)
+		frame = draw_face(frame, bndbox, predict, verbose=True)
 		cv2.imshow('frame', frame)
 
 		if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -63,7 +116,3 @@ if __name__ == "__main__":
 
 	vid.release()
 	cv2.destroyAllWindows()
-	
-
-
-
